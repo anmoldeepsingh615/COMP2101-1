5b841f61cb0410b460fbb598c476e0c1b490b03b
rajvirsingh0066 <ajvirsingh0066@gmail.com>
2019-05-30 13:13:26 -0700

    Final

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -11,14 +11,17 @@
 
 echo "Setuid files:"
 echo "============="
+
 find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 3
 echo "============================================================================================================="
 echo "Task 1 Find Setgid files:"
+
 echo "============================================================================================================="
 find / -type f -executable -perm -2000 -ls 2>/dev/null | sort -k 3
 echo "============================================================================================================="
 echo "Task 2 for the exercise is to modify it to also display the 10 largest files in the system, sorted by their sizes"
 echo "============================================================================================================="
+
 #find / 2>/dev/null -type f -exec lsw -l --block-size=M {} + | sort -rh -k 5 | head -n 10 | awk '{print $5, $3, $9}'
 find /home/ -type f -exec ls -alh  --block-size=M {} \; | sort -hr -k5 | head -n 10 | awk '{print $5, $3, $9}'
 echo ""
